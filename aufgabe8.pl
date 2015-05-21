6defee4d065380305a4f8d8e96f86de966eee720
Markus Klemm <markus@markusklemm.net>
2015-05-21 03:30:35 +0200

    Fertsch

--- aufgabe8.pl
+++ aufgabe8.pl
@@ -15,6 +15,6 @@ tochter(anke,jan,eva).
 kind(X,Y,Z) :- sohn(X,Y,Z) ; tochter(X,Y,Z).
 bruder(X,Y) :- sohn(X,Z1,Z2), kind(Y,Z1,Z2), X\== Y.
 schwester(X,Y) :- tochter(X,Z1,Z2), kind(Y,Z1,Z2), X\== Y.
-enkel(EN,E1) :- kind(EN,K1,K2), kind(K1,E1,E2), kind(K2,E1,E2).
+enkel(EN,E1) :- kind(EN,_K1,_K2), kind(_K1,E1,_E2).
 onkel(B1,K) :- bruder(B1,E1), kind(K,E1,_E2).
 tante(B1,K) :- schwester(B1,E1), kind(K,E1,_E2).
