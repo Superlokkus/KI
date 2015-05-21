53cd3181c90703362943ce39acb478f9fe05d64f
Markus Klemm <markus@markusklemm.net>
2015-05-21 03:15:40 +0200

    Verwandtschaft fixen

--- aufgabe15.pl
+++ aufgabe15.pl
@@ -26,21 +26,22 @@ sindbeziehung(P1,P2,In,Antwort) :-
 (verheiratet(P1,P2), match(verheiratet,In,Rest),append([ja],Rest,Antwort));
 (not(verheiratet(P1,P2)), match(verheiratet,In,Rest),append([nein],Rest,Antwort));
 (verwandt(P1,P2), match(verwandt,In,Rest),append([ja],Rest,Antwort));
-(not(verwandt(P1,P2)), match(verwandt,In,Rest),append([nein],Rest,Antwort));.
+(not(verwandt(P1,P2)), match(verwandt,In,Rest),append([nein],Rest,Antwort)).
 istbeziehung(P1,P2,In,Antwort) :-
 (bruder(P1,P2), match(bruder,In,Rest),append([ja],Rest,Antwort));
 (not(bruder(P1,P2)), match(bruder,In,Rest),append([nein],Rest,Antwort));
 (schwester(P1,P2), match(schwester,In,Rest),append([ja],Rest,Antwort));
 (not(schwester(P1,P2)), match(schwester,In,Rest),append([nein],Rest,Antwort)).
 
-istSatz(In,Antwort) :- (match(ist,In,R1), person(P1,R1,R2), artikel(R2,R3),
+istSatz1(In,Antwort) :- match(ist,In,R1), person(P1,R1,R2), artikel(R2,R3),
 beziehungswort(B,R3,R4), match(von,R4,R5), 
-person(P2,R5,R6), istbeziehung(P1,P2,B,Antwort));
-(match(ist,In,R1), person(P1,R1,R2), match(mit,R2,R3),
-person(P2,R3,R6), sindbeziehung(P1,P2,B,Antwort)).
+person(P2,R5,R6), istbeziehung(P1,P2,B,Antwort).
+istSatz2(In,Antwort) :- match(ist,In,R1), person(P1,R1,R2), match(mit,R2,R3),
+person(P2,R3,R6), sindbeziehung(P1,P2,R6,Antwort).
 
 sindSatz(In,Antwort) :- 
 match(sind,In,R1), person(P1,R1,R2), match(und,R2,R3), 
 person(P2,R3,R4), paarverb(R4,R5), sindbeziehung(P1,P2,R5,Antwort).
 
-frage(Frage,Antwort) :- sindSatz(Frage,Antwort); istSatz(Frage,Antwort).
+frage(Frage,Antwort) :- sindSatz(Frage,Antwort); 
+istSatz1(Frage,Antwort);istSatz2(Frage,Antwort).
