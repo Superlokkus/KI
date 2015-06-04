b2cfa862d9e3a85ecf92a2208e670f755b306330
Markus Klemm <markus@markusklemm.net>
2015-06-04 11:17:47 +0200

    Sollte eigentlich gehen, aber endlos schleife

--- aufgabe20.pl
+++ aufgabe20.pl
@@ -3,26 +3,48 @@
 adj(X,Y) :- adj0(X,Y); adj0(Y,X).
 
 ort(X) :- member(X,[berlin,dresden]).
+
 adj0([STA,STM,SA,SM,ZA,ZM,Ort1], [STA,STM,SA,SM,ZA,ZM,Ort2]) :-
 	number(STA),number(STM),number(SA),number(SM), number(ZA), number(ZM),
 	ort(Ort1), ort(Ort2), (SA > 0; SM > 0). %Fahren
 
 adj0([STA,STM,SA,SM,ZA,ZM,Ort], 
-	[NSTA,NSTM,NSA,NSM,ZA,ZM,Ort]) :- 
-	number(STA),number(STM),number(SA),number(SM), number(ZA), number(ZM),
-	STA > 0, STM >0, ort(Ort),
-	NSTA is STA-1,NSTM is STM-1,NSA is SA+1,NSM is SM+1. %Einsteigen Dual Start
+	[NSTA,NSTM,NSA,NSM,NZA,NZM,Ort]) :- 
+	number(STA),number(STM),number(SA),number(SM), number(ZA), number(ZM),number(NSA),number(NSM),
+	start(Ort),
+	(NSTA =< NSTM; NSTM =:= 0), (NSA =< NSM; NSM =:=0), (NZA =< NZM; NZM =:=0),
+	(NSA + NSM) =< 2, 
+	(
+		(STA + SA =:= NSTA + NSA),
+		(STM + SM =:= NSTM + NSM),
+		ZA = NZA,
+		ZM = NZM
+	).
+	 			%Transfer Start
+
 adj0([STA,STM,SA,SM,ZA,ZM,Ort], 
-	[STA,STM,NSA,NSM,NZA,NZM,Ort]) :- 
-	number(STA),number(STM),number(SA),number(SM), number(ZA), number(ZM),
-	SA >0, SM >0, ort(Ort),
-	NSA is SA-1,NSM is SM-1,NZA is ZA+1,NZM is ZM+1. %Austeigen Dual Ziel
-%adj0([STA,STM,SA,SM,ZA,ZM,Ort], [STA-1,STM-1,SA+1,SM+1,ZA,ZM,Ort]) :- 	 . %Einsteigen
+	[NSTA,NSTM,NSA,NSM,NZA,NZM,Ort]) :- 
+	number(STA),number(STM),number(SA),number(SM), number(ZA), number(ZM),number(NSA),number(NSM),
+	ziel(Ort),
+	(NSTA =< NSTM; NSTM =:= 0), (NSA =< NSM; NSM =:=0), (NZA =< NZM; NZM =:=0),
+	(NSA + NSM) =< 2, 
+	(
+		(ZA + SA =:= NZA + NSA),
+		(ZM + SM =:= NZM + NSM),
+		STA = NSTA,
+		STM = NSTM
+	).
+	 			%Transfer ziel
 
+ziel(dresden).
+start(berlin).
 
-goal([0,0,0,0,3,3,dresden]).
+%ausgang [3,3,0,0,0,0,berlin] 
+%ziel [0,0,0,0,3,3,dresden] 
+%solution([3,3,0,0,0,0,berlin],[0,0,0,0,3,3,dresden],X).
+%(STA > 0, NSTA is STA -1, NSA is SA +1);
+%		(STM > 0, NSTM is STM -1, NSM is SM +1)
+
 % Bedeutung Liste: [Anzahl Autonome StartOrt, Anzahl Finanzminister StartOrt,
 % Anz. A. Smart, Anz. FM Smart, Anz. A. Ziel, Anz. FM Ziel, Ort Smart]
-solution(Path) :- Start = [3,3,0,0,0,0,berlin], 
-goal(Goal), idDfs(Start,Goal,Path).
- 
+solution(Ausgang,Ziel,Path) :- idDfs(Ausgang,Ziel,Path).
