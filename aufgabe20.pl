390361fb5c7b2a6848593b53505d2fa7fbe1ac20
Markus Klemm <markus@markusklemm.net>
2015-06-04 09:42:54 +0200

    Wohl is statement

--- aufgabe20.pl
+++ aufgabe20.pl
@@ -6,9 +6,9 @@ ort(X) :- member(X,[berlin,dresden]).
 adj0([STA,STM,SA,SM,ZA,ZM,Ort1], [STA,STM,SA,SM,ZA,ZM,Ort2]) :- 
 	ort(Ort1), ort(Ort2), (SA > 0; SM > 0). %Fahren
 
-adj0([STA,STM,SA,SM,ZA,ZM,Ort], [_ is STA-1,_ is STM-1,SA+1,SM+1,ZA,ZM,Ort]) :- 
+adj0([STA,STM,SA,SM,ZA,ZM,Ort], [_ is STA-1,_ is STM-1,_ is SA+1,_ isSM+1,ZA,ZM,Ort]) :- 
 	STA > 0, STM >0. %Einsteigen Dual Start
-adj0([STA,STM,SA,SM,ZA,ZM,Ort], [STA,STM,SA-1,SM-1,ZA+1,ZM+1,Ort]) :- 
+adj0([STA,STM,SA,SM,ZA,ZM,Ort], [STA,STM,_ is SA-1,_ is SM-1,_ is ZA+1,_ is ZM+1,Ort]) :- 
 	SA >0, SM >0 . %Austeigen Dual Ziel
 %adj0([STA,STM,SA,SM,ZA,ZM,Ort], [STA-1,STM-1,SA+1,SM+1,ZA,ZM,Ort]) :- 	 . %Einsteigen
 
@@ -18,5 +18,4 @@ goal([0,0,0,0,3,3,dresden]).
 % Anz. A. Smart, Anz. FM Smart, Anz. A. Ziel, Anz. FM Ziel, Ort Smart]
 solution(Path) :- Start = [3,3,0,0,0,0,berlin], 
 goal(Goal), idDfs(Start,Goal,Path).
-
-test(X,Y) :- Y is X +1. 
+ 
